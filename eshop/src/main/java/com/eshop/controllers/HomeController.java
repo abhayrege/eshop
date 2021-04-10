7c2c4368bc3d9f0601e2038045eb107de673a760
abhayrege <abhayrege@localhost>
2021-04-10 21:12:33 +0530

    general cleanup

--- eshop/src/main/java/com/eshop/controllers/HomeController.java
+++ eshop/src/main/java/com/eshop/controllers/HomeController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/")
@@ -12,9 +11,6 @@ public class HomeController {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String home(ModelMap modelMap) {
-		System.out.println("HomeController...method home..........................................");
-		System.out.println("new output");
-		//ModelAndView mav = new ModelAndView("home");
 		return "index";
 	}
 
